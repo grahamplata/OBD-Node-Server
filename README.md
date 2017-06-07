eead2a0f5a9c670e4cb2a91377d14c835c5af1f3
Graham Plata <graham.plata@gmail.com>
2017-06-06 23:14:56 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+## OBD-Dash
+
+A javascript server for handling realtime sensor data from OBD-II vehicle ports. Works with ELM327 OBD-II adapters, and is fit for the Raspberry Pi.
+
+## Motivation
+
+My motivation for this project was to find a use for my raspberry Pi. Like many other Pi owners I tend to buy more than I use. I was inspired by this [project](https://www.youtube.com/watch?v=sUa8MFtMCP0) and wanted to see if I could go a little bit further.
+
+## Installation
+-----------
+
+```Javascript
+git clone https://github.com/grahamplata/OBD_Server.git
+
+npm install
+```
+
+## Basic Usage
+-----------
+
+```Javascript
+git clone https://github.com/grahamplata/OBD_Server.git
+
+npm install
+
+npm run dev // For synthetic data output
+npm run start // For usage with OBDII
+
+```
---
+++ server.js
@@ -0,0 +1,93 @@
+// imports
+var app = require('http').createServer();
+var io = require('socket.io').listen(app);
+var port = 3000;
+var fs = require('fs');
+var OBD = require('obd-parser');
+var path = require('path');
+
+
+// If developement mode use fake-obd, if production use comm
+var getConnector = require('obd-parser-serial-connection');
+if (process.env.NODE_ENV != "development") {
+    var getConnector = require('obd-parser-development-connection');
+}
+
+// Serial connections settings
+var connect = getConnector({
+    serialPath: '/dev/rfcomm0',
+    serialOpts: {
+        baudrate: 115200
+    }
+});
+
+//globals
+var rpm = 0;
+var mph = 0;
+var coolantTemp = 0;
+
+io.on('connection', function (socket) {
+
+
+    console.log('New client connected!\n----\n');
+    // OBD functions upon connection
+    OBD.init(connect).then(function () {
+
+        // initialize data pollers
+        var rpmPoller = new OBD.ECUPoller({
+            pid: new OBD.PIDS.Rpm(),
+            interval: 1000
+        });
+        var vehicleSpeedPoller = new OBD.ECUPoller({
+            pid: new OBD.PIDS.VehicleSpeed(),
+            interval: 1000
+        });
+        var coolantTempPoller = new OBD.ECUPoller({
+            pid: new OBD.PIDS.CoolantTemp(),
+            interval: 100
+        });
+
+        //ready
+        console.log('OBDII connected and ready');
+
+        // Poll for vehicle RPM and pass value to global
+        rpmPoller.on('data', function (output) {
+            rpm = output.value;
+        });
+
+        vehicleSpeedPoller.on('data', function (output) {
+            mph = output.value;
+        });
+
+        coolantTempPoller.on('data', function (output) {
+            coolantTemp = output.value;
+        });
+
+        // Start Vehicle Polling
+        console.log('Starting Polling');
+        vehicleSpeedPoller.startPolling();
+        rpmPoller.startPolling();
+        coolantTempPoller.startPolling();
+
+    });
+
+    setInterval(function () {
+        io.emit('new_data', {
+            rpm: rpm,
+            mph: mph,
+            coolantTemp: coolantTemp
+        });
+        console.log('Data Emitted! --- ' + 'rpm: ' + rpm + ' mph: ' + mph + ' Temp: ' + coolantTemp);
+    }, 1000);
+
+});
+
+// on disconnect functions
+io.on('disconnect', function () {
+    socket.emit('disconnected', socket.id);
+    console.log('Client has lost connection!');
+});
+
+
+app.listen(port);
+console.log("listening on port: ", port);
